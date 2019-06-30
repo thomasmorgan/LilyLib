bbaafb6c18c34b3b131c2c31318e52d6dadc515a
thomasmorgan <thomas.j.h.morgan@gmail.com>
2019-06-30 14:10:57 +0100

    Finish melody 2

--- vgs1.py
+++ vgs1.py
@@ -26,12 +26,14 @@ class vgs1(Manuscript):
 		interval = self.interval
 		second = self.second
 		third = self.third
-		fourth = self.fourth
 		third_b = self.third_b
+		fourth = self.fourth
 		sixth_b = self.sixth_b
 		rhythm = self.rhythm
 		scale = self.scale
+		notes = self.notes
 		descending_melody = self.descending_melody
+		wandering_melody = self.wandering_melody
 
 		self.sections["intro"].score["rh"] = [R("2.")]*2
 		self.sections["intro"].score["lh"] = LH()*3 + LH(b=C(['g', 'a', 'c`']), c='ef')
@@ -46,7 +48,11 @@ class vgs1(Manuscript):
 		self.sections["melody1"].score["lh"] = LH_loop()
 
 		self.sections["melody2"].score["rh"] = [N("d`", duration="4."), R("4")] + descending_melody()[0:7]
-		self.sections["melody2"].score["lh"] = LH_loop()[0:18] + LH(b=third('bf'), c='g') + LH(b=second('bf', key=DMinorH()), c='g') + LH(a='f,', b=fourth('a'), c='f')*2 + LH(b=third('bf'), c='g') + LH(a='a,', b=N('cs`'), c='a') + LH(a='d', b=fourth('a'), c='f')*2
+		self.sections["melody2"].score["lh"] = LH_loop()[0:15]
+
+		self.key = DMinorH()
+		self.sections["melody2"].score["rh"] += wandering_melody()
+		self.sections["melody2"].score["lh"] += LH_loop()[15:18] + LH(b=third('bf'), c='g') + LH(b=second('bf', key=DMinorH()), c='g') + LH(a='f,', b=fourth('a'), c='f')*2 + LH(b=third('bf'), c='g') + LH(a='a,', b=N('cs`'), c='a') + LH(a='d', b=fourth('a'), c='f')*2
 
 		print(self)
 
@@ -73,6 +79,17 @@ class vgs1(Manuscript):
 		ornament = self.rhythm([16, 16, 8], self.sixth_b(self.notes(['ef`', 'f`', 'g`'])))
 		melody[-2:-1] = ornament
 		return melody
+
+	def wandering_melody(self):
+		def basic_scale():
+			return self.rhythm([8], self.scale('g`', 'd``') + self.scale('f``', 'd``'))
+		first_pass = [self.third_b('g`')] + basic_scale() + self.rhythm([4, 8], self.notes(['a``', 'f``', 'd``']))
+		first_pass[6] = self.fifth_b(first_pass[6])
+		first_pass[9] = self.fourth_b(first_pass[9])
+
+		second_pass = basic_scale()[1:] + self.rhythm(["4."], [self.third_b('d``')]) + [self.rest("4")]
+		second_pass[2:7] = self.third_b(second_pass[2:7])
+		return first_pass + second_pass
 		
 
 vgs1()
