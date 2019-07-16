7104e981513d164b280fdc75b55bea001973db37
thomasmorgan <thomas.j.h.morgan@gmail.com>
2019-07-16 10:11:49 +0100

    Use better writing method, not W

--- vgs1.py
+++ vgs1.py
@@ -13,7 +13,7 @@ tempo = t68()
 
 # create staves and sections
 staves = [Treble("rh"), Bass("lh")]
-sections = create_sections(["intro", "octaves", "in_d", "melody1", "melody2"])
+score = create_sections(["intro", "octaves", "in_d", "melody1", "melody2"])
 
 # Define common motifs
 def LH(a='g,', b=third('g'), c='d'):
@@ -38,53 +38,39 @@ def wandering_melody():
 
 # write the various sections
 
-W("intro", "rh",
-	M("r r").r('2.'))
-W("intro", "lh",
-	LH()*3 + LH(b=C('a', [-1, 0, 2]), c='ef'))
+score["intro"]["rh"] = M("r r").r('2.')
+score["intro"]["lh"] = LH()*3 + LH(b=C('a', [-1, 0, 2]), c='ef')
 
-W("octaves", "rh",
-	M('d` f` f` ef`').r('4.'))
-W("octaves", "lh",
-	LH(a=O('g,,')) + LH(a=O('f,,'), b=third('a'), c='f') + LH(a=O('bf,,'), b=third('bf'), c='f') + LH(a=O('c,'), b=fourth('g'), c='ef'))
+score["octaves"]["rh"] = M('d` f` f` ef`').r('4.')
+score["octaves"]["lh"] = LH(a=O('g,,')) + LH(a=O('f,,'), b=third('a'), c='f') + LH(a=O('bf,,'), b=third('bf'), c='f') + LH(a=O('c,'), b=fourth('g'), c='ef')
 
 key = GMinorH()
-W("in_d", "rh",
-	M('d` r').r('2.'))
-W("in_d", "lh",
-	LH('d,')*2 + LH('d,', third('a')) + LH('d,', third('fs')))
+score["in_d"]["rh"] = M('d` r').r('2.')
+score["in_d"]["lh"] = LH('d,')*2 + LH('d,', third('a')) + LH('d,', third('fs'))
 
-W("melody1", "rh",
-	M("r r").r('4.', 4) + descending_melody())
-
+score["melody1"]["rh"] = M("r r").r('4.', 4) + descending_melody()
 key = GMinor()
 ending = M('d` ' + 'ef` '*4).r(8, 4).h([0] + [-2]*4)
 ornament = M('ef` f` g`').r(16, 16, 8).h(-2)
 ending[-2:-1] = ornament
-W("melody1", "rh",
-	ending)
+score["melody1"]["rh"] += ending
 
 key = GMinorH()
-W("melody1", "lh",
-	LH_loop())
+score["melody1"]["lh"] = LH_loop()
 
-W("melody2", "rh",
-	M("d` r").r("4.", 4) + M(descending_melody()[0:7]))
-W("melody2", "lh",
-	M(LH_loop()[0:15]))
+score["melody2"]["rh"] = M("d` r").r("4.", 4) + M(descending_melody()[0:7])
+score["melody2"]["lh"] = M(LH_loop()[0:15])
 key = DMinorH()
-W("melody2", "rh", 
-	wandering_melody())
-W("melody2", "lh",
-	M(LH_loop()[15:18]) + LH('g,', third('bf'), 'g') + LH('g,', second('bf'), 'g') + LH('f,', fourth('a'), 'f')*2 + LH('g,', third('bf'), 'g') + LH('a,', 'cs`', 'a') + LH('d', fourth('a'), 'f')*2)
+score["melody2"]["rh"] += wandering_melody()
+score["melody2"]["lh"] += M(LH_loop()[15:18]) + LH('g,', third('bf'), 'g') + LH('g,', second('bf'), 'g') + LH('f,', fourth('a'), 'f')*2 + LH('g,', third('bf'), 'g') + LH('a,', 'cs`', 'a') + LH('d', fourth('a'), 'f')*2
 
-key = CMinorH()
-bridge_v1 = M('f`` d`` c`` b` c`` g` af` b` c`` g` af` b`').r(4, 8) + M('c``').r(2)
-bridge_v2 = M('r f` g`').r(8)*2 + third('r c` r r d` r')*2 + M('r ef` g`')*2
+# key = CMinorH()
+# bridge_v1 = M('f`` d`` c`` b` c`` g` af` b` c`` g` af` b`').r(4, 8) + M('c``').r(2)
+# bridge_v2 = M('r f` g`').r(8)*2 + third('r c` r r d` r')*2 + M('r ef` g`')*2
 
-key = GMinorH()
-bridge_v1 += M('r').r(8) + M('c`` ef`` a` ef`` a`').r(8, 4) + M('ef``').r(2)
-bridge_v2 += M('r ef` fs`')*3
+# key = GMinorH()
+# bridge_v1 += M('r').r(8) + M('c`` ef`` a` ef`` a`').r(8, 4) + M('ef``').r(2)
+# bridge_v2 += M('r ef` fs`')*3
 
 #W("bridge", "rh",
 #	M('d``').r(8))# + V + bridge_v1 + CV + bridge_v2 + EV
