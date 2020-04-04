f0f0c43cc92facd4e2afd9d4b5aca3e01ab36434
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2020-04-04 13:40:26 -0700

    Test Pitch class

--- tests.py
+++ tests.py
@@ -1,14 +1,18 @@
-from models import Letter
+from models import Letter, Pitch
 
-print("Testing Letter class...", end="")
+print("Testing Letter class", end="")
 
 letter = Letter('a')
 assert isinstance(letter, Letter)
 assert letter.letter == 'a'
 
+print(".", end="")
+
 letter = Letter('b')
 letter = Letter('ff')
 letter = Letter('css')
+
+print(".", end="")
 
 try:
     letter = Letter('fa')
@@ -16,5 +20,28 @@ except ValueError:
     pass
 else:
     raise ValueError("Error, letter fa created. This should not be possible.")
+
+print(" passed!")
+
+print("Testing Pitch class", end="")
+
+pitch = Pitch(',,')
+assert isinstance(pitch, Pitch)
+assert pitch.pitch == ',,'
+
+print(".", end="")
+
+pitch = Pitch('')
+pitch = Pitch(',')
+pitch = Pitch('``')
+
+print(".", end="")
+
+try:
+    pitch = Pitch(',`')
+except ValueError:
+    pass
+else:
+    raise ValueError("Error, letter ,` created. This should not be possible.")
 
 print(" passed!")
