8c7c3fbde841a5714c7aef71ab1cb6ada34178fd
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2020-04-06 16:27:04 -0700

    Fix util.flatten

--- util.py
+++ util.py
@@ -5,12 +5,12 @@ def print_error(message):
 def flatten(List):
     while any([isinstance(i, list) or isinstance(i, tuple) for i in List]):
         new_list = []
-    for i in List:
-        if isinstance(i, list) or isinstance(i, tuple):
-            new_list += i
-        else:
-            new_list += [i]
-            List = new_list
+        for i in List:
+            if isinstance(i, list) or isinstance(i, tuple):
+                new_list += i
+            else:
+                new_list += [i]
+                List = new_list
     return List
 
 
