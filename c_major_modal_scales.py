61a5524a1a4b685f24d1f7bb387b3ce3ab9b1dba
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2020-04-10 20:28:01 -0700

    Update scales demo to use piece.notes

--- c_major_modal_scales.py
+++ c_major_modal_scales.py
@@ -8,20 +8,22 @@ class CMajorModalScales(Piece):
     def __init__(self):
         super().__init__()
         self.title = "C Major Modal Scales"
-        self.create_sections("basic", "scale", "looped", "super")
+        self.create_sections("basic", "notes", "scale", "looped", "super")
 
         self.score["treble"]["basic"] = [Note("c`", 8), Note("d`", 8), Note("e`", 8), Note("f`", 8), Note("g`", 8), Note("a`", 8), Note("b`", 8), Note("c``", 8)]
         self.score["bass"]["basic"] = [Note("c", 8), Note("d", 8), Note("e", 8), Note("f", 8), Note("g", 8), Note("a", 8), Note("b", 8), Note("c`", 8)]
 
-        self.score["treble"]["scale"] = self.scale('d`', 'd``', dur=8)
-        self.score["bass"]["scale"] = self.scale('d', 'd`', dur=8)
+        self.score["treble"]["notes"] = self.notes('d` e` f` g` a` b` c`` d``', dur=8)
+        self.score["bass"]["notes"] = self.notes('d e f g a b c` d`', dur=8)
+
+        self.score["treble"]["scale"] = self.scale('e`', 'e``', dur=8)
+        self.score["bass"]["scale"] = self.scale('e', 'e`', dur=8)
 
         self.score["treble"]["looped"] = []
-        for start in ['e`', 'f`', 'g`', 'a`', 'b`', 'c``']:
-            self.score["treble"]["looped"] += [Note(t, 8) for t in self.key.scale(start, start + '`')]
         self.score["bass"]["looped"] = []
-        for start in ['e', 'f', 'g', 'a', 'b', 'c`']:
-            self.score["bass"]["looped"] += [Note(t, 8) for t in self.key.scale(start, start + '`')]
+        for start in ['f`', 'g`', 'a`', 'b`', 'c``']:
+            self.score["treble"]["looped"] += [Note(t, 8) for t in self.key.scale(start, start + '`')]
+            self.score["bass"]["looped"] += [Note(t, 8) for t in self.key.scale(start[:-1], start)]
 
         keys = [CMajor, BMajor, AMajor, GMajor, FMajor, EMajor, DMajor, CMajor]
         self.score["treble"]["super"] = []
