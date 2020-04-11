827cd81f85820ade75f80fca9f7b19490a9681bc
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2020-04-11 13:20:07 -0700

    Create a print all demos file

---
+++ print_all_demos.py
@@ -0,0 +1,9 @@
+from demo_middle_c import MiddleC
+from demo_c_major_chord import CMajorChord
+from demo_c_major_scale import CMajorScale
+from demo_c_major_modal_scales import CMajorModalScales
+
+MiddleC()
+CMajorChord()
+CMajorScale()
+CMajorModalScales()
