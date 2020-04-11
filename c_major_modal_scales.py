d4af67566a15d56d6f94a8d6098a1d8fcba3302a
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2020-04-10 20:16:35 -0700

    Update scales demo to use piece.scale

--- c_major_modal_scales.py
+++ c_major_modal_scales.py
@@ -1,5 +1,6 @@
 from piece import Piece
 from models import Note
+from keys import CMajor, DMajor, EMajor, FMajor, GMajor, AMajor, BMajor
 
 
 class CMajorModalScales(Piece):
@@ -12,8 +13,8 @@ class CMajorModalScales(Piece):
         self.score["treble"]["basic"] = [Note("c`", 8), Note("d`", 8), Note("e`", 8), Note("f`", 8), Note("g`", 8), Note("a`", 8), Note("b`", 8), Note("c``", 8)]
         self.score["bass"]["basic"] = [Note("c", 8), Note("d", 8), Note("e", 8), Note("f", 8), Note("g", 8), Note("a", 8), Note("b", 8), Note("c`", 8)]
 
-        self.score["treble"]["scale"] = [Note(t, 8) for t in self.key.scale('d`', 'd``')]
-        self.score["bass"]["scale"] = [Note(t, 8) for t in self.key.scale('d', 'd`')]
+        self.score["treble"]["scale"] = self.scale('d`', 'd``', dur=8)
+        self.score["bass"]["scale"] = self.scale('d', 'd`', dur=8)
 
         self.score["treble"]["looped"] = []
         for start in ['e`', 'f`', 'g`', 'a`', 'b`', 'c``']:
@@ -22,12 +23,13 @@ class CMajorModalScales(Piece):
         for start in ['e', 'f', 'g', 'a', 'b', 'c`']:
             self.score["bass"]["looped"] += [Note(t, 8) for t in self.key.scale(start, start + '`')]
 
+        keys = [CMajor, BMajor, AMajor, GMajor, FMajor, EMajor, DMajor, CMajor]
         self.score["treble"]["super"] = []
-        for start in self.key.scale('c```', 'c``'):
-            self.score["treble"]["super"] += [Note(t, 8) for t in self.key.scale(str(start), str(start)[:-1])]
         self.score["bass"]["super"] = []
-        for start in self.key.scale('c``', 'c`'):
-            self.score["bass"]["super"] += [Note(t, 8) for t in self.key.scale(str(start), str(start)[:-1])]
+        for start, key in zip(self.key.scale('c```', 'c``'), keys):
+            self.score["treble"]["super"] += self.scale(str(start), str(start)[:-1], key=key, dur=8)
+            self.score["bass"]["super"] += self.scale(str(start)[:-1], str(start)[:-2], key=key, dur=8)
+
         print(self)
 
 
