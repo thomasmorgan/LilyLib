99b3d575964e9e40cbb3c751f325362390698972
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2020-08-30 17:46:56 -0700

    Extensively destory lilylib.py

--- lilylib.py
+++ lilylib.py
@@ -1,8 +1,6 @@
-from models import *
-from util import *
-from keys import *
+from keys import CMajor
+from models import Section, Stave, Note
 from tempi import *
-from staves import *
 
 title = ""
 subtitle = ""
@@ -11,216 +9,219 @@ opus = ""
 staves = []
 tempo = None
 key = CMajor()
+score = None
+
 
 def header():
-	return (
-		'\\version "2.18.2"\n' +
-		'\\language "english"\n' +
-		'\\header {\n' +
-		'    title = "{}"\n'.format(title) + 
-		'    subtitle = "{}"\n'.format(subtitle) +
-		'    composer = "{}"\n'.format(composer) +
-		'    mutopiacomposer = ""\n' +
-		'    mutopiainstrument = "piano"\n' +
-		'    source = ""\n' +
-		'    style = "Romatic"\n' +
-		'    license = "Creative Commons Attribution-ShareAlike 4.0"\n' +
-		'    maintainer = "Anonymous"\n' +
-		'    opus = "{}"\n'.format(opus) +
-		'}\n'
-	)
+    return (
+        '\\version "2.18.2"\n'
+        + '\\language "english"\n'
+        + '\\header {\n'
+        + '    title = "{}"\n'.format(title)
+        + '    subtitle = "{}"\n'.format(subtitle)
+        + '    composer = "{}"\n'.format(composer)
+        + '    mutopiacomposer = ""\n'
+        + '    mutopiainstrument = "piano"\n'
+        + '    source = ""\n'
+        + '    style = "Romatic"\n'
+        + '    license = "Creative Commons Attribution-ShareAlike 4.0"\n'
+        + '    maintainer = "Anonymous"\n'
+        + '    opus = "{}"\n'.format(opus)
+        + '}\n'
+    )
+
 
 def start_score():
-	return('\\score { <<\n')
+    return('\\score { <<\n')
+
 
 def end_score():
-	return('>> }\n')
+    return('>> }\n')
 
-def notes(notes):
-	notes = notes.split(" ")
-	new_notes = []
-	for note in notes:
-		if note:
-			new_notes.append(Note(note))
-	return new_notes
 
-def notify(n):
-	if isinstance(n, str):
-		return notes(n)
-	if isinstance(n, Melody):
-		return n.notes
-	return n
+# def notes(notes):
+# 	notes = notes.split(" ")
+# 	new_notes = []
+# 	for note in notes:
+# 		if note:
+# 			new_notes.append(Note(note))
+# 	return new_notes
 
-def scale(start, stop):
-	if not isinstance(start, Note):
-		start = Note(start)
-	if not isinstance(stop, Note):
-		stop = Note(stop)
+# def notify(n):
+# 	if isinstance(n, str):
+# 		return notes(n)
+# 	if isinstance(n, Melody):
+# 		return n.notes
+# 	return n
+
+# def scale(start, stop):
+# 	if not isinstance(start, Note):
+# 		start = Note(start)
+# 	if not isinstance(stop, Note):
+# 		stop = Note(stop)
 	
-	index_of_start = [i for i, n in enumerate(key.notes) if n.letter == start.letter and n.pitch == start.pitch][0]
-	index_of_stop = [i for i, n in enumerate(key.notes) if n.letter == stop.letter and n.pitch == stop.pitch][0]
+# 	index_of_start = [i for i, n in enumerate(key.notes) if n.letter == start.letter and n.pitch == start.pitch][0]
+# 	index_of_stop = [i for i, n in enumerate(key.notes) if n.letter == stop.letter and n.pitch == stop.pitch][0]
 
-	if index_of_stop > index_of_start:
-		notes = key.notes[index_of_start:index_of_stop + 1]
-	else:
-		notes = key.notes[index_of_stop:index_of_start + 1]
-		notes.reverse()
+# 	if index_of_stop > index_of_start:
+# 		notes = key.notes[index_of_start:index_of_stop + 1]
+# 	else:
+# 		notes = key.notes[index_of_stop:index_of_start + 1]
+# 		notes.reverse()
 
-	new_notes = []
-	for n in notes:
-		new_notes.append(Note(n.letter + n.pitch))
+# 	new_notes = []
+# 	for n in notes:
+# 		new_notes.append(Note(n.letter + n.pitch))
 
-	return M(new_notes)
+# 	return M(new_notes)
 
-S = scale
+# S = scale
 
-def chord(root, notes):
-	if not isinstance(root, Note):
-		root = Note(root)
+# def chord(root, notes):
+# 	if not isinstance(root, Note):
+# 		root = Note(root)
 
-	if not key.includes(root.letter):
-		print_error("Cannot build chord on {}as it is not in {}".format(note, key.name))
+# 	if not key.includes(root.letter):
+# 		print_error("Cannot build chord on {}as it is not in {}".format(note, key.name))
 
-	index_of_root = [i for i, n in enumerate(key.notes) if n.letter == root.letter and n.pitch == root.pitch][0]
+# 	index_of_root = [i for i, n in enumerate(key.notes) if n.letter == root.letter and n.pitch == root.pitch][0]
 
-	new_notes = []
-	for note in notes:
-		new_note = key.notes[index_of_root + note]
-		new_notes.append(new_note)
+# 	new_notes = []
+# 	for note in notes:
+# 		new_note = key.notes[index_of_root + note]
+# 		new_notes.append(new_note)
 
-	chord = Chord(new_notes)
-	return M(chord)
+# 	chord = Chord(new_notes)
+# 	return M(chord)
 
-C = chord
+# C = chord
 
-def melody(*notes):
-	new_notes = []
-	for n in notes:
-		new_notes.append(notify(n))
-	new_notes = flatten(new_notes)
-	return Melody(key, new_notes)
+# def melody(*notes):
+# 	new_notes = []
+# 	for n in notes:
+# 		new_notes.append(notify(n))
+# 	new_notes = flatten(new_notes)
+# 	return Melody(key, new_notes)
 
-M = melody
+# M = melody
 
-def interval(notes, size):
-	if isinstance(notes, str):
-		notes = notes.split(" ")
-	if isinstance(notes, Note):
-		notes = [notes]
+# def interval(notes, size):
+# 	if isinstance(notes, str):
+# 		notes = notes.split(" ")
+# 	if isinstance(notes, Note):
+# 		notes = [notes]
 
-	new_notes = []
-	for note in notes:
-		if note:
-			if not isinstance(note, Note):
-				note = Note(note)
+# 	new_notes = []
+# 	for note in notes:
+# 		if note:
+# 			if not isinstance(note, Note):
+# 				note = Note(note)
 
-			if note.letter == "r":
-				new_notes.append(note)
-			else:
-				if not key.includes(note.letter):
-					print_error("Cannot build interval on {} as it is not in {}".format(note, key.name))
+# 			if note.letter == "r":
+# 				new_notes.append(note)
+# 			else:
+# 				if not key.includes(note.letter):
+# 					print_error("Cannot build interval on {} as it is not in {}".format(note, key.name))
 				
-				index_of_note = [i for i, n in enumerate(key.notes) if n.letter == note.letter and n.pitch == note.pitch][0]
-				new_note = key.notes[index_of_note + size]
-				new_chord = Chord(note.chord_repr() + " " + new_note.chord_repr())
-				new_chord.dur = note.dur
-				new_notes.append(new_chord)
-	return M(new_notes)
+# 				index_of_note = [i for i, n in enumerate(key.notes) if n.letter == note.letter and n.pitch == note.pitch][0]
+# 				new_note = key.notes[index_of_note + size]
+# 				new_chord = Chord(note.chord_repr() + " " + new_note.chord_repr())
+# 				new_chord.dur = note.dur
+# 				new_notes.append(new_chord)
+# 	return M(new_notes)
 
-def second(note):
-	return interval(note, 1)
+# def second(note):
+# 	return interval(note, 1)
 
-_2nd = second
+# _2nd = second
 
-def third(note):
-	return interval(note, 2)
+# def third(note):
+# 	return interval(note, 2)
 
-_3rd = third
+# _3rd = third
 
-def third_b(note):
-	return interval(note, -5)
+# def third_b(note):
+# 	return interval(note, -5)
 
-_3rd_ = third_b
+# _3rd_ = third_b
 
-def fourth(note):
-	return interval(note, 3)
+# def fourth(note):
+# 	return interval(note, 3)
 
-_4th = fourth
+# _4th = fourth
 
-def fourth_b(note):
-	return interval(note, -4)
+# def fourth_b(note):
+# 	return interval(note, -4)
 
-_4th_ = fourth_b
+# _4th_ = fourth_b
 
-def fifth(note):
-	return interval(note, 4)
+# def fifth(note):
+# 	return interval(note, 4)
 
-_5th = fifth
+# _5th = fifth
 
-def fifth_b(note):
-	return interval(note, -3)
+# def fifth_b(note):
+# 	return interval(note, -3)
 
-_5th_ = fifth_b
+# _5th_ = fifth_b
 
-def sixth(note):
-	return interval(note, 5)
+# def sixth(note):
+# 	return interval(note, 5)
 
-_6th = sixth
+# _6th = sixth
 
-def sixth_b(note):
-	return interval(note, -2)
+# def sixth_b(note):
+# 	return interval(note, -2)
 
-_6th_ = sixth_b
+# _6th_ = sixth_b
 
-def seventh(note):
-	return interval(note, 6)
+# def seventh(note):
+# 	return interval(note, 6)
 
-_7th = seventh
+# _7th = seventh
 
-def octave(note):
-	return interval(note, 7)
+# def octave(note):
+# 	return interval(note, 7)
 
-O = octave
+# O = octave
+
 
 def create_sections(names):
-	score = {}
-	for name in names:
-		score[name] = Section(name=name, staves=staves)
-	return score
+    score = {}
+    for name in names:
+        score[name] = Section(name=name, staves=staves)
+    return score
 
 
-def rest(*durs):
-	return M("r "*len(durs)).r(durs)
+# def rest(*durs):
+# 	return M("r "*len(durs)).r(durs)
 
-R = rest
+# R = rest
 
-def voices():
-	return(M(notes("\n<<\n{")))
+# def voices():
+# 	return(M(notes("\n<<\n{")))
 
-V = voices()
+# V = voices()
 
-def change_voice():
-	return(M(notes("} \\\\ {")))
+# def change_voice():
+# 	return(M(notes("} \\\\ {")))
 
-CV = change_voice()
+# CV = change_voice()
 
-def end_voices():
-	return(M(notes("}\n>>\n")))
+# def end_voices():
+# 	return(M(notes("}\n>>\n")))
 
-EV = end_voices()
+# EV = end_voices()
+
 
 def print_score():
-	printed_score = header() + start_score()
-	for stave in staves:
-		printed_score += stave.start()
-		printed_score += str(key)
-		printed_score += str(tempo)
-		for section in score:
-			printed_score += score[section].print_stave(stave.name)
-		printed_score += stave.end()
-	printed_score += end_score()
-	print(printed_score)
-
-
-
-
+    printed_score = header() + start_score()
+    for stave in staves:
+        printed_score += stave.start
+        printed_score += str(key)
+        printed_score += str(tempo)
+        for section in score:
+            printed_score += score[section].print_stave(stave.clef)
+        printed_score += stave.end
+    printed_score += end_score()
+    print(printed_score)
