f02518a6a8ec925cbf3b044c85d520ccbc517c52
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2020-09-06 15:40:02 -0700

    Update c major scale demo

--- demo_c_major_scale.py
+++ demo_c_major_scale.py
@@ -1,5 +1,4 @@
 from piece import Piece
-from models import Note
 
 
 class CMajorScale(Piece):
@@ -8,8 +7,8 @@ class CMajorScale(Piece):
         self.title = "C Major Scale"
 
     def write_score(self):
-        self.score["treble"] = [Note("c`", 8), Note("d`", 8), Note("e`", 8), Note("f`", 8), Note("g`", 8), Note("a`", 8), Note("b`", 8), Note("c``", 8)]
-        self.score["bass"] = [Note("c", 8), Note("d", 8), Note("e", 8), Note("f", 8), Note("g", 8), Note("a", 8), Note("b", 8), Note("c`", 8)]
+        self.score["treble"] = self.scale("c`", "c``", 8)
+        self.score["bass"] = self.scale("c", 8, 8)
 
 
 CMajorScale()
