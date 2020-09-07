e1c9357fd529d21c254da90157fa1d9dc16f1cbc
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2020-09-07 11:11:54 -0700

    Update modal scales demo

--- demo_c_major_modal_scales.py
+++ demo_c_major_modal_scales.py
@@ -27,22 +27,18 @@ class CMajorModalScales(Piece):
         }
 
         # The looped section programmatically builds a series of scales
-        # Note the start values are just strings, and so they can be modified by adding or removing `
-        # Note also that the score ends up being a list-of-lists-of-notes, but nested lists are fine
+        # Note that the bass clef is just a tansposition of the treble clef
         looped = {"treble": []}
         for start in self.scale('f`', 'c``', 8):
             looped["treble"] += self.scale(start, 8, 8)
         looped["bass"] = self.transpose(looped["treble"], -1)
 
         # The smart section programmatically builds a series of scales in different keys
-        # Note the use of key.scale to build a series of tones to start each scale but that self.scale it used to actually build the scales.
-        # This is because key.scale returns a scale of durationless tones, while self.scale returns a scale of proper Notes
-        # Note also the use of Piece.transpose() to change the pitches of the Tones that define the limits of the scale
-        # Finally, note how we use zip and list comprehension to avoid the need for a for loop, and use * 2 to double the scales in the treble clef
+        # Note how we use zip and list comprehension to avoid the need for a for loop, and use step = 2 to play every other note in the treble clef
         keys = [CMajor, BMajor, AMajor, GMajor, FMajor, EMajor, DMajor, CMajor]
         start_notes = self.scale('c```', 'c``', 8)
         smart = {
-            "treble": [self.scale(start, -8, 16, key=key) * 2 for start, key in zip(start_notes, keys)],
+            "treble": [self.scale(start, -8, 8, step=2, key=key) for start, key in zip(start_notes, keys)],
             "bass": [self.scale(self.transpose(start, -1), -8, 8, key=key) for start, key in zip(start_notes, keys)]
         }
 
