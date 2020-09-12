89c9d1c811af53c4dac0f8363ccaaca06ef6e53e
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2020-09-12 11:08:51 -0700

    Use motif function to build mad rush

--- demo_mad_rush.py
+++ demo_mad_rush.py
@@ -1,5 +1,5 @@
 from piece import Piece
-from util import flatten, pattern, subset
+from util import flatten, pattern
 
 
 class MadRush(Piece):
@@ -7,10 +7,13 @@ class MadRush(Piece):
     def details(self):
         self.title = "Mad Rush"
         self.composer = "Philip Glass"
-        self.tempo = "4/4"
         self.key = "F Major"
 
     def write_score(self):
+
+        root_chord = self.arpeggio(self.key.root, 6)
+        iii_chord = [self.transpose(t, -1, "scale") if t.letter == self.key.root else t for t in root_chord]
+        iii7_chord = [self.transpose(t, 1, "scale") if t.letter == self.key.root else t for t in root_chord]
 
         def triplet_bar(note_pair, bars=1):
             return self.triplets(self.notes(note_pair, 8) * int(6 * bars))
@@ -27,38 +30,57 @@ class MadRush(Piece):
         def arpeggio2(arp):
             return arpeggio1(arp, tempo=False) + self.tempo_change("14/8") + pattern(arp, 1, 2, 3, 4, 3, 4, 3, 2, 1, 2, 3, 4, 3, 2) * 2
 
-        home_chord = self.arpeggio(self.key.root, 6)
-        minor_third = self.arpeggio(self.transpose(self.key.root, -1, mode="scale"), 6, key=self.iii)
+        def A_motif(chord, bars, *tweaks):
+            if chord == "root":
+                motif = {
+                    'treble': triplet_bar(pattern(root_chord, 6, 5), bars=bars),
+                    'bass1': doublet_bar(pattern(root_chord, 2, 3), bars=bars)
+                }
+            elif chord == "iii":
+                motif = {
+                    'treble': triplet_bar(pattern(iii_chord, 6, 4), bars=bars),
+                    'bass1': doublet_bar(pattern(iii_chord, 2, 3), bars=bars)
+                }
+                if 'low first' in tweaks:
+                    motif['bass1'][0] = iii_chord[0]
+            elif chord == "iii7":
+                motif = {
+                    'treble': triplet_bar(pattern(iii7_chord, 6, 4), bars=bars),
+                    'bass1': doublet_bar(pattern(iii7_chord, 2, 3), bars=bars)
+                }
+
+            return motif
+
+        def merge(*motifs):
+            combined = {
+                'treble': [],
+                'bass1': []
+            }
+            for motif in motifs:
+                combined['treble'] += motif['treble']
+                combined['bass1'] += motif['bass1']
+            return combined
 
         sections = {}
 
-        sections['A1'] = {}
-        chord1 = subset(home_chord, 1, 3)
-        chord2 = [self.iii.mediant + ','] + self.arpeggio(self.transpose(self.key.root, -1, mode="scale"), 3, key=self.iii)
         sections['A1'] = {
             'treble': self.rests(1) * 4,
-            'bass1': doublet_bar(chord1[-2:], bars=2) + self.notes([chord2[1], chord2[3]], 8) + doublet_bar(chord2[2:], 1.75),
-            'bass2': self.notes([chord1[0]] * 2 + [chord2[0]] * 2, 1, "~ ")
+            'bass1': merge(A_motif('root', 2), A_motif('iii', 2, 'low first'))['bass1'],
+            'bass2': self.notes(pattern(root_chord, 1, 1) + self.transpose(pattern(iii_chord, 3, 3), -2), 1, "~ ")
         }
 
-        sections['A2'] = {}
-        chord1 = home_chord
-        chord2 = minor_third
-        sections['A2'] = {
-            'treble': triplet_bar(pattern(chord1, 6, 5), bars=2) + triplet_bar(pattern(chord2, 6, 4), bars=2),
-            'bass1': doublet_bar(chord1[1:3], bars=2) + doublet_bar(chord2[1:3], bars=2),
-            'bass2': self.notes([chord1[0]] * 2 + [chord2[0]] * 2, 1, "~ ")
-        }
+        sections['A2'] = merge(A_motif('root', 2), A_motif('iii', 2))
+        sections['A2']['bass2'] = self.notes(pattern(root_chord, 1, 1) + pattern(iii_chord, 1, 1), 1, "~ ")
 
-        sections['A3'] = {}
-        chord1 = home_chord
-        chord1b = self.arpeggio7(self.iii.subtonic, 7, key=self.iii)
-        chord2 = minor_third
-        sections['A3'] = {
-            'treble': triplet_bar(chord1[-2:], bars=1) + triplet_bar([chord1b[-3], chord1b[-1]], bars=0.5) + triplet_bar(chord1[-2:], bars=0.5) + triplet_bar([chord2[3], chord2[5]], bars=2),
-            'bass1': doublet_bar(chord1[1:3], bars=1) + doublet_bar(chord1b[1:3], bars=0.5) + doublet_bar(chord1[1:3], bars=0.5) + doublet_bar(chord2[1:3], bars=2),
-            'bass2': self.notes(chord1[0], 1) + self.notes([chord1b[0], chord1b[0], chord1[0], chord1[0]], 4) + self.notes([chord2[0]] * 2, 1, "~ ")
-        }
+        sections['A3'] = merge(A_motif('root', 1), A_motif('iii7', 0.5), A_motif('root', 0.5), A_motif('iii', 2))
+        sections['A3']['bass2'] = self.notes(pattern(root_chord, 1), 1) + self.notes(pattern(iii7_chord, 1, 1) + pattern(root_chord, 1, 1), 4) + self.notes(pattern(iii_chord, 1, 1), 1, "~ ")
+
+        # sections['A3'] = {}
+        # sections['A3'] = {
+        #     'treble': triplet_bar(pattern(root_chord, 6, 5), bars=1) + triplet_bar(pattern(iii7_chord, 7, 5), bars=0.5) + triplet_bar(pattern(root_chord, 6, 5), bars=0.5) + triplet_bar(pattern(iii_chord, 6, 4), bars=2),
+        #     'bass1': doublet_bar(pattern(root_chord, 2, 3), bars=1) + doublet_bar(pattern(iii7_chord, 2, 3), bars=0.5) + doublet_bar(pattern(root_chord, 2, 3), bars=0.5) + doublet_bar(pattern(iii_chord, 2, 3), bars=2),
+        #     'bass2': self.notes(pattern(root_chord, 1), 1) + self.notes(pattern(iii7_chord, 1, 1) + pattern(root_chord, 1, 1), 4) + self.notes(pattern(iii_chord, 1) * 2, 1, "~ ")
+        # }
 
         #     'A3': {
         #         'treble': self.repeat(triplet_bar("c`` a`") + triplet_bar('c`` g`', 0.5) + triplet_bar("c`` a`", 0.5) + triplet_bar('c`` e`', 2)),
@@ -100,7 +122,7 @@ class MadRush(Piece):
         for section in sections:
             self.name(sections[section]['treble'], section)
 
-        A = ['A1']
+        A = ['A1', 'A2', 'A3']
 
         structure = [A]
 
