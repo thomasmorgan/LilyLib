80519f0412f856824ae85cfc28a7b775386dc787
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2020-11-09 13:53:33 -0700

    piece: don't import Note

--- piece.py
+++ piece.py
@@ -1,4 +1,4 @@
-from models import Note, Chord, Key
+from models import Chord, Key
 from keys import key_dictionary
 from staves import Treble, Bass
 from util import flatten, split_and_flatten, all_tones, tonify, all_pitches, separate, equivalent_letters, pitch, letter, equivalent_tone
