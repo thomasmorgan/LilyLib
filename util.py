7d46cfb8abe776b20bef0dcbd9a2899152bb34bb
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2020-11-09 21:13:29 -0700

    'r' tone no longer exists

--- util.py
+++ util.py
@@ -25,7 +25,7 @@ all_accents = ['ff', 'f', '', 's', 'ss']
 all_letters = flatten([[letter + accent for accent in all_accents] for letter in all_base_letters])
 
 all_pitches = [",,,", ",,", ",", "", "`", "``", "```"]
-all_tones = flatten([[letter + pitch for letter in all_letters] for pitch in all_pitches] + ['r'])
+all_tones = flatten([[letter + pitch for letter in all_letters] for pitch in all_pitches])
 
 all_durs = ['\\longa' '\\breve', 1, 2, 4, 8, 16, 32, 64, 128, '1.', '2.', '4.', '8.', '16.', '32.', '64.', '128.', '1..', '2..', '4..', '8..', '16..', '32..', '64..', '128..']
 
