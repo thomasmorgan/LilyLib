a6d70e16a12a5ceed02af43f7fa446d3f2e712ed
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2020-11-11 10:53:20 -0700

    lilyib: move functions from piece to lilylib

--- piece.py
+++ piece.py
@@ -112,52 +112,6 @@ class Piece:
         else:
             raise TypeError("stave with value {} cannot be printed".format(stave))
 
-    def rest(self, dur):
-        """ Returns a list with a single Point that prints as a rest with the specified duration. """
-        return [Point([], dur)]
-
-    def rests(self, *dur):
-        """ Returns a list of Points that print as rests with the specified durations. """
-        return flatten([self.rest(d) for d in flatten(dur)])
-
-    def note(self, tone, dur, ornamentation=""):
-        """ Returns a list with a single Point that prints as a note with the specified tone and duration.
-        Passing an empty string or list as tone will return a rest.
-        Passing a list of tones (or a string that can be split into multiple tones raises an error. """
-        tone = flatten([tonify(tone)])
-        if len(tone) > 1:
-            raise ValueError("Cannot create single note with tone of {}.".format(tone))
-        return [Point(tone, dur, ornamentation)]
-
-    def notes(self, tones, dur, ornamentation=""):
-        """ Returns a list of Points that print as notes with the specified tone(s) and duration(s).
-        If tones is a string including adjacent white space, rests are produced.
-        If tones is a list including empty lists or empty strings, rests are produced.
-        Nested lists of tones are not permitted as tones will not be flattened to avoid removing empty lists."""
-        tones = tonify(tones)
-        tones = [tones] if isinstance(tones, str) else tones
-        dur = split_and_flatten(dur)
-        orn = split_and_flatten(ornamentation) if '"' not in ornamentation else flatten([ornamentation])
-
-        max_length = max([len(tones), len(dur), len(orn)])
-
-        zip_list = zip(range(max_length), cycle(tones), cycle(dur), cycle(orn))
-        return flatten([self.note(t, d, o) for i, t, d, o in zip_list])
-
-    def chord(self, tones, dur, ornamentation=""):
-        """ Returns a list containing a single Point that prints as a chord with the specified tones and duration. """
-        tones = flatten([tonify(tones)])
-        return [Point(tones, dur, ornamentation)]
-
-    def chords(self, tones, dur, ornamentation=""):
-        dur = split_and_flatten(dur)
-        orn = split_and_flatten(ornamentation) if '"' not in ornamentation else flatten([ornamentation])
-
-        max_length = max([len(tones), len(dur), len(orn)])
-
-        zip_list = zip(range(max_length), cycle(tones), cycle(dur), cycle(orn))
-        return flatten([self.chord(t, d, o) for i, t, d, o in zip_list])
-
     def series(self, tones, start, stop_or_length, dur=None, step=1):
         tones = tonify(tones)
         start, stop_or_length = self.validate_series_args(tones, start, stop_or_length, dur, step)
@@ -204,31 +158,6 @@ class Piece:
             raise ValueError("series step must be an int, not {}".format(step))
 
         return start, stop_or_length
-
-    def add(self, point, tones, *tweaks):
-        if isinstance(point, list):
-            for subpoint in point:
-                self.add(subpoint, tones)
-        elif isinstance(point, Point):
-            if not point.is_rest or "include rests" in tweaks:
-                point.add(tones)
-        return point
-
-    def remove(self, point, tones):
-        if isinstance(point, list):
-            for subpoint in point:
-                self.remove(subpoint, tones)
-        elif isinstance(point, Point):
-            point.remove(tones)
-        return point
-
-    def replace(self, point, old_tones, new_tones):
-        if isinstance(point, list):
-            for subitem in point:
-                self.replace(subitem, old_tones, new_tones)
-        elif isinstance(point, Point):
-            point.replace(old_tones, new_tones)
-        return point
 
     def make_stop_inclusive(self, start, stop):
         if stop >= start:
