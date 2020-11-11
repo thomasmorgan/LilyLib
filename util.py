253be4c41d362aca9ac0cb63e2b44332477f6bd3
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2020-11-11 10:40:37 -0700

    tone: Move tone functions from util to tone

--- util.py
+++ util.py
@@ -19,113 +19,11 @@ def split_and_flatten(item):
     return flatten([subitem.split(" ") if isinstance(subitem, str) else subitem for subitem in item])
 
 
-all_base_letters = ['c', 'd', 'e', 'f', 'g', 'a', 'b']
-all_accents = ['ff', 'f', '', 's', 'ss']
-
-all_letters = flatten([[letter + accent for accent in all_accents] for letter in all_base_letters])
-
-all_pitches = [",,,", ",,", ",", "", "`", "``", "```"]
-all_tones = flatten([[letter + pitch for letter in all_letters] for pitch in all_pitches])
-
-all_durs = ['\\longa' '\\breve', 1, 2, 4, 8, 16, 32, 64, 128, '1.', '2.', '4.', '8.', '16.', '32.', '64.', '128.', '1..', '2..', '4..', '8..', '16..', '32..', '64..', '128..']
-
-
-equivalent_letters = {
-    'cf': 'b',
-    'c': 'bs',
-    'cs': 'df',
-    'df': 'cs',
-    'd': 'd',
-    'ds': 'ef',
-    'ef': 'ds',
-    'e': 'ff',
-    'es': 'f',
-    'ff': 'e',
-    'f': 'es',
-    'fs': 'gf',
-    'gf': 'fs',
-    'g': 'g',
-    'gs': 'af',
-    'af': 'gs',
-    'a': 'a',
-    'as': 'bf',
-    'bf': 'as',
-    'b': 'cf',
-    'bs': 'c'
-}
-
-
-def separate(tone):
-    tone = tonify(tone)
-    if tone[-1] in ["`", ","]:
-        split = tone.split(tone[-1], 1)
-        return split[0], split[1] + tone[-1]
-    else:
-        return tone, ''
-
-
 linebreak = ['\\break\n']
 
 
 def clef(clef):
     return ['\\clef {}'.format(clef)]
-
-
-def pitch(tone):
-    return separate(tone)[1]
-
-
-def letter(tone):
-    return separate(tone)[0]
-
-
-def accent(tone):
-    let = letter(tone)
-    if len(let) == 1:
-        return ''
-    else:
-        return let[-1]
-
-
-def base_letter(tone):
-    return letter(tone)[0]
-
-
-def equivalent_tone(tone):
-    new_letter = equivalent_letters[letter(tone)]
-    if base_letter(tone) == 'c' and base_letter(new_letter) == 'b':
-        new_pitch = all_pitches[all_pitches.index(pitch(tone)) - 1]
-    elif base_letter(tone) == 'b' and base_letter(new_letter) == 'c':
-        new_pitch = all_pitches[all_pitches.index(pitch(tone)) + 1]
-    else:
-        new_pitch = pitch(tone)
-    new_tone = new_letter + new_pitch
-    return new_tone
-
-
-def tonify(item):
-    """ Returns an unflattened list of valid tones and empty lists.
-
-    Multi-tone strings are split into lists of valid tones. A seris of N spaces is
-    converted into a seris of N-1 empty lists. These produce rests when assigned to
-    Points, but will be erased by flattening the list. """
-
-    if isinstance(item, list):
-        return [tonify(subitem) for subitem in item]
-    elif isinstance(item, str):
-        if " " in item:
-            split_tones = item.split(" ")
-            split_tones = [tone if tone != '' else [] for tone in split_tones]
-            return tonify(split_tones)
-        else:
-            if item not in all_tones:
-                raise ValueError("{} is not a valid tone.".format(item))
-            return item
-    else:
-        try:
-            return item.tone
-        except AttributeError:
-            raise ValueError("Cannot tonify {}".format(item))
 
 
 def select(List, *indexes):
