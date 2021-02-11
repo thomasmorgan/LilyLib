c89323dd83e1452fa13e0f9d63c73cc2abb2158a
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2021-02-11 11:27:19 -0700

    demos: update prelude in c for no markup

--- demos/demo_prelude_in_c.py
+++ demos/demo_prelude_in_c.py
@@ -1,7 +1,7 @@
 from piece import Piece
-from util import pattern, select, omit
+from util import pattern, select, omit, rep
 from tones import tonify
-from points import note, notes, chord, rest, rests, dominant7, arpeggio, arpeggio7, diminished7
+from points import note, notes, chord, tied_note, rest, dominant7, arpeggio, arpeggio7, diminished7
 from markup import voices
 
 
@@ -17,8 +17,8 @@ class PreludeInC(Piece):
 
         def motif(tones):
             tones = tonify(tones)
-            self.score["bass"] += 2 * voices(rest(16) + notes(select(tones, 2), ['8.', 4], "~ "), note(select(tones, 1), 2))
-            self.score["treble"] += 2 * (rest(8) + notes(pattern(tones, 2 * [3, 4, 5]), 16))
+            self.score["bass"] += rep(voices(rest(16) + tied_note(select(tones, 2), ['8.', 4]), note(select(tones, 1), 2)), 2)
+            self.score["treble"] += rep(rest(8) + notes(pattern(tones, 2 * [3, 4, 5]), 16), 2)
 
         bar = [''] * 40
 
@@ -38,7 +38,7 @@ class PreludeInC(Piece):
         bar[12] = select(diminished7('g', 7, 'G Minor'), 1, 2, 4, 5, 7)
 
         bar[13] = omit(arpeggio('f', 'd``', 'D Minor'), 4)
-        bar[14] = omit(diminished7('f', 'b`', 'D Minor'), 3, 6)
+        bar[14] = omit(diminished7('f', 'cf``', 'D Minor'), 3, 6)
         bar[15] = self.transpose(bar[13], -1)
         bar[16] = omit(arpeggio7('e', 'f`', 'F Major'), 5)
 
@@ -48,14 +48,14 @@ class PreludeInC(Piece):
         bar[20] = omit(self.dominant7('c', 'e`'), 2)
 
         bar[21] = omit(arpeggio7('f,', 'e`', 'F Major'), 2, 3, 4)
-        bar[22] = omit(diminished7('fs,', 'ef`', 'A Major'), 2, 4, 5)
+        bar[22] = omit(diminished7('gf,', 'ef`', 'A Major'), 2, 4, 5)
         bar[23] = 'af, f b c` d`'
         bar[24] = omit(dominant7('g,', 'd`', 'G Major'), 2, 3)
 
         bar[25] = omit(self.arpeggio('g,', 'e`'), 2)
         bar[26] = 'g, d g c` f`'
         bar[27] = omit(dominant7('g,', 'f`', 'G Major'), 2, 4, 7)
-        bar[28] = ['g,'] + omit(diminished7('ef', 'fs`', 'A Major'), 2, 5)
+        bar[28] = ['g,'] + omit(diminished7('ef', 'gf`', 'A Major'), 2, 5)
 
         bar[29] = omit(self.arpeggio('g,', 'g`'), 2, 6)
         bar[30] = bar[26]
@@ -68,7 +68,7 @@ class PreludeInC(Piece):
 
         def held_bass(tones):
             tones = tonify(tones)
-            return voices(rest(16) + notes(tones[1], ['8.', 4, 2], "~ ~ "), note(tones[0], 1))
+            return voices(rest(16) + tied_note(tones[1], ['8.', 4, 2]), note(tones[0], 1))
 
         def long_melody(tones):
             tones = tonify(tones)
@@ -78,8 +78,8 @@ class PreludeInC(Piece):
         self.score['treble'] += long_melody(dominant7('g`', 'f``', 'G Major')) + pattern(self.scale('d`', 'f`', 16), 1, 3, 2, 1)
         self.score["bass"] += held_bass('c, c') + held_bass('c, b,')
 
-        self.score['treble'] += [chord(self.arpeggio('e`', 'c``'), 1)]
-        self.score['bass'] += [chord('c, c', 1)]
+        self.score['treble'] += chord(self.arpeggio('e`', 'c``'), 1)
+        self.score['bass'] += chord('c, c', 1)
 
     def end_score(self):
         return ('>>\n  \\layout {\n \\context {\n \\Score\n \\override SpacingSpanner.common-shortest-duration =\n #(ly:make-moment 1/8)\n }\n }\n }')
