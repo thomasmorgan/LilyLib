ddea37762ab681ea156420ed3f30f219e0dd7cad
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2021-02-11 14:14:14 -0700

    demos: refactor mad rush

--- demos/demo_mad_rush.py
+++ demos/demo_mad_rush.py
@@ -10,7 +10,7 @@ class MadRush(Piece):
         self.title = "Mad Rush"
         self.composer = "Philip Glass"
         self.key = "F Major"
-        self.summary = False
+        self.summary = True
 
     def subtext(self):
         if self.summary:
@@ -36,16 +36,28 @@ class MadRush(Piece):
         else:
             return ""
 
+    def create_chords(self):
+        self.aI = self.arpeggio('f', 6)
+        self.aiii = arpeggio('e', 6, 'A Minor')
+        self.aiii7 = pattern(arpeggio7('g', 7, 'A Minor'), 1, 2, 3, 5, 6, 7)
+        self.aii = arpeggio('g', 6, 'G Minor')
+        self.aii7 = ['f'] + subset(self.aii, 2, 6)
+
+        self.bI7 = self.arpeggio('f,', 4) + self.arpeggio7('a`', 4)
+        self.biii = arpeggio('e,', 4, 'A Minor') + arpeggio('a`', 4, 'A minor')
+        self.biii7 = [self.transpose(t, i) for t, i in zip(self.bI7, [1, 0, 0, 1, -1, 0, 0, 0])]
+        self.bii7 = arpeggio('g,', 4, 'G Minor') + arpeggio('g`', 4, 'G Minor')
+        self.bii7d5 = [self.transpose(t, i, 'semitone') for t, i in zip(self.bii7, [0, 0, -1, 0, 0, 0, -1, 0])]
+
+        self.diii = arpeggio('a`', 3, 'A Minor')
+        self.dI = ['f``', 'f``']
+        self.dii = ['f``', 'df``', 'c``']
+
     def write_score(self):
+        self.create_chords()
         self.score["treble"] = []
         self.score["bass"] = []
         sections = {}
-
-        aI = self.arpeggio('f', 6)
-        aiii = arpeggio('e', 6, 'A Minor')
-        aiii7 = pattern(arpeggio7('g', 7, 'A Minor'), 1, 2, 3, 5, 6, 7)
-        aii = arpeggio('g', 6, 'G Minor')
-        aii7 = ['f'] + subset(aii, 2, 6)
 
         def triplet_bar(note_pair, bars=1):
             return triplets(rep(notes(note_pair, 8), int(6 * bars)))
@@ -58,9 +70,9 @@ class MadRush(Piece):
 
             if 'no treble' in tweaks:
                 motif['treble'] = rep(rest(1), bars)
-            elif chord == aI:
+            elif chord == self.aI:
                 motif['treble'] = triplet_bar(pattern(chord, [6, 5]), bars=bars)
-            elif chord == aii and 'low triplets' in tweaks:
+            elif chord == self.aii and 'low triplets' in tweaks:
                 motif['treble'] = triplet_bar(pattern(chord, [5, 4]), bars=bars)
             else:
                 motif['treble'] = triplet_bar(pattern(chord, [6, 4]), bars=bars)
@@ -80,20 +92,14 @@ class MadRush(Piece):
 
             return motif
 
-        sections['A0'] = join(A_motif(aI, 2, 'no treble'), A_motif(aiii, 2, 'no treble', 'low first'))
+        sections['A0'] = join(A_motif(self.aI, 2, 'no treble'), A_motif(self.aiii, 2, 'no treble', 'low first'))
         sections['A0']['treble'] = time_signature('4/4', sections['A0']['treble'])
-        sections['A1'] = join(A_motif(aI, 2), A_motif(aiii, 2))
-        sections['A2'] = join(A_motif(aI, 1), A_motif(aiii7, 0.5, 'crotchet bass'), A_motif(aI, 0.5, 'crotchet bass'), A_motif(aiii, 2))
-        sections['A3'] = join(A_motif(aii, 1, 'low triplets', 'extend tie'), A_motif(aii, 1), A_motif(aI, 2))
-        sections['A4'] = join(A_motif(aii, 1, 'low triplets'), A_motif(aii7, 0.5, 'crotchet bass'), A_motif(aii, 0.5, 'crotchet bass'), A_motif(aI, 2))
+        sections['A1'] = join(A_motif(self.aI, 2), A_motif(self.aiii, 2))
+        sections['A2'] = join(A_motif(self.aI, 1), A_motif(self.aiii7, 0.5, 'crotchet bass'), A_motif(self.aI, 0.5, 'crotchet bass'), A_motif(self.aiii, 2))
+        sections['A3'] = join(A_motif(self.aii, 1, 'low triplets', 'extend tie'), A_motif(self.aii, 1), A_motif(self.aI, 2))
+        sections['A4'] = join(A_motif(self.aii, 1, 'low triplets'), A_motif(self.aii7, 0.5, 'crotchet bass'), A_motif(self.aii, 0.5, 'crotchet bass'), A_motif(self.aI, 2))
 
         A = ['A1', 'A1', 'A2', 'A2', 'A2', 'A3', 'A3', 'A4']
-
-        bI7 = self.arpeggio('f,', 4) + self.arpeggio7('a`', 4)
-        biii = arpeggio('e,', 4, 'A Minor') + arpeggio('a`', 4, 'A minor')
-        biii7 = [self.transpose(t, i) for t, i in zip(bI7, [1, 0, 0, 1, -1, 0, 0, 0])]
-        bii7 = arpeggio('g,', 4, 'G Minor') + arpeggio('g`', 4, 'G Minor')
-        bii7d5 = [self.transpose(t, i, 'semitone') for t, i in zip(bii7, [0, 0, -1, 0, 0, 0, -1, 0])]
 
         def arpeggio_bar(arp, bars):
             return rep(notes(pattern(arp, 1, 2, 3, 4, 3, 2), 16), int(4 * bars))
@@ -113,10 +119,10 @@ class MadRush(Piece):
 
             return motif
 
-        sections['B1'] = join(B_motif(bI7, 2), B_motif(biii, 1), B_motif(biii, 1, 'alt'))
-        sections['B2'] = join(B_motif(bI7, 1), B_motif(biii7, 0.5), B_motif(bI7, 0.5), B_motif(biii, 1), B_motif(biii, 1, 'alt'))
-        sections['B3'] = join(B_motif(bii7, 2), B_motif(bI7, 1), B_motif(bI7, 1, 'alt'))
-        sections['B4'] = join(B_motif(bii7, 1), B_motif(bii7d5, 1), B_motif(bI7, 1), B_motif(bI7, 1, 'alt'))
+        sections['B1'] = join(B_motif(self.bI7, 2), B_motif(self.biii, 1), B_motif(self.biii, 1, 'alt'))
+        sections['B2'] = join(B_motif(self.bI7, 1), B_motif(self.biii7, 0.5), B_motif(self.bI7, 0.5), B_motif(self.biii, 1), B_motif(self.biii, 1, 'alt'))
+        sections['B3'] = join(B_motif(self.bii7, 2), B_motif(self.bI7, 1), B_motif(self.bI7, 1, 'alt'))
+        sections['B4'] = join(B_motif(self.bii7, 1), B_motif(self.bii7d5, 1), B_motif(self.bI7, 1), B_motif(self.bI7, 1, 'alt'))
 
         sections['B1']['treble'][0] = time_signature('12/8', sections['B1']['treble'][0])[0]
         sections['B2']['treble'][0] = time_signature('12/8', sections['B2']['treble'][0])[0]
@@ -150,14 +156,10 @@ class MadRush(Piece):
                 motif['treble'] = self.harmonize(tied_note(chord[0], [1, 1]) + tied_note(chord[1], [1, 1]), 1, 'octave')
             return motif
 
-        diii = arpeggio('a`', 3, 'A Minor')
-        dI = ['f``', 'f``']
-        dii = ['f``', 'df``', 'c``']
-
-        sections['D1'] = D_motif(diii, 'A1')
-        sections['D2'] = D_motif(diii, 'A2')
-        sections['D3'] = D_motif(dI * 2, 'A3')
-        sections['D4'] = D_motif(dii, 'A4')
+        sections['D1'] = D_motif(self.diii, 'A1')
+        sections['D2'] = D_motif(self.diii, 'A2')
+        sections['D3'] = D_motif(self.dI * 2, 'A3')
+        sections['D4'] = D_motif(self.dii, 'A4')
 
         D = ['D1', 'D1', 'D2', 'D2', 'D2', 'D3', 'D3', 'D4']
 
