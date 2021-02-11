e8e699e7a878aa0e3a617efea91c64d452355f75
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2021-02-11 11:40:44 -0700

    demos: prelude in c simple: update for no markup and refactor

--- demos/demo_prelude_in_c_simple.py
+++ demos/demo_prelude_in_c_simple.py
@@ -1,7 +1,7 @@
 from piece import Piece
-from util import pattern, select
+from util import pattern, select, rep, join
 from tones import tonify
-from points import note, notes, chord, rest
+from points import note, notes, chord, rest, tied_note
 from markup import voices
 
 
@@ -12,14 +12,15 @@ class PreludeInCSimple(Piece):
         self.composer = "J. S. Bach"
         self.opus = "BVW 846"
 
-    def write_score(self):
-        self.score["treble"], self.score["bass"] = [], []
+    def motif(self, c):
+        tones = tonify(c)
+        return {
+            'treble': rep(rest(8) + notes(pattern(tones, 2 * [3, 4, 5]), 16), 2),
+            'bass': rep(voices(rest(16) + tied_note(select(tones, 2), ['8.', 4]), note(select(tones, 1), 2)), 2)
+        }
 
-        def motif(c):
-            tones = tonify(c)
-            self.score["bass"] += 2 * voices(rest(16) + notes(select(tones, 2), ['8.', 4], "~ "), note(select(tones, 1), 2))
-            self.score["treble"] += 2 * (rest(8) + notes(pattern(tones, 2 * [3, 4, 5]), 16))
-
+    @property
+    def chords(self):
         bar = [''] * 40
 
         bar[1] = 'c` e` g` c`` e``'
@@ -62,24 +63,25 @@ class PreludeInCSimple(Piece):
         bar[31] = 'g, d g b f`'
         bar[32] = 'c, c g bf e`'
 
-        for c in bar:
-            if c:
-                motif(c)
+        return bar
 
-        def held_bass(tones):
-            tones = tonify(tones)
-            return voices(rest(16) + notes(tones[1], ['8.', 4, 2], "~ ~ "), note(tones[0], 1))
+    def held_bass(self, tones):
+        tones = tonify(tones)
+        return voices(rest(16) + tied_note(tones[1], ['8.', 4, 2]), note(tones[0], 1))
 
-        def long_melody(tones):
-            tones = tonify(tones)
-            return rest(8) + notes(pattern(tones, 1, 2, 3, 4, 3, 2, 3, 2, 1, 2), 16)
+    def long_melody(self, tones):
+        tones = tonify(tones)
+        return rest(8) + notes(pattern(tones, 1, 2, 3, 4, 3, 2, 3, 2, 1, 2), 16)
 
-        self.score['treble'] += long_melody('f a c` f`') + notes('f d', 16) * 2
-        self.score['treble'] += long_melody('g` b` d`` f``') + pattern(self.scale('d`', 'f`', 16), 1, 3, 2, 1)
-        self.score["bass"] += held_bass('c, c') + held_bass('c, b,')
+    def write_score(self):
+        self.score = join([self.motif(chord) for chord in self.chords[1:33]])
 
-        self.score['treble'] += [chord('e` g` c`', 1)]
-        self.score['bass'] += [chord('c, c', 1)]
+        self.score['treble'] += self.long_melody('f a c` f`') + notes('f d', 16) * 2
+        self.score['treble'] += self.long_melody('g` b` d`` f``') + pattern(self.scale('d`', 'f`', 16), 1, 3, 2, 1)
+        self.score['bass'] += self.held_bass('c, c') + self.held_bass('c, b,')
+
+        self.score['treble'] += chord('e` g` c`', 1)
+        self.score['bass'] += chord('c, c', 1)
 
 
 if __name__ == "__main__":
