bca5645189bbf38c2723b876dc9c37f707aee814
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2021-02-11 12:07:06 -0700

    demos: prelude in c: inherit from simple

--- demos/demo_prelude_in_c.py
+++ demos/demo_prelude_in_c.py
@@ -1,31 +1,19 @@
-from piece import Piece
-from util import pattern, select, omit, rep
-from tones import tonify
-from points import note, notes, chord, tied_note, rest, dominant7, arpeggio, arpeggio7, diminished7
-from markup import voices
+from util import select, omit
+from points import dominant7, arpeggio, arpeggio7, diminished7
+from demos.demo_prelude_in_c_simple import PreludeInCSimple
+from copy import deepcopy
 
 
-class PreludeInC(Piece):
+class PreludeInC(PreludeInCSimple):
 
-    def details(self):
-        self.title = "Prelude in C"
-        self.composer = "J. S. Bach"
-        self.opus = "BVW 846"
-
-    def write_score(self):
-        self.score["treble"], self.score["bass"] = [], []
-
-        def motif(tones):
-            tones = tonify(tones)
-            self.score["bass"] += rep(voices(rest(16) + tied_note(select(tones, 2), ['8.', 4]), note(select(tones, 1), 2)), 2)
-            self.score["treble"] += rep(rest(8) + notes(pattern(tones, 2 * [3, 4, 5]), 16), 2)
-
+    @property
+    def chords(self):
         bar = [''] * 40
 
         bar[1] = self.arpeggio('c`', 'e``')
         bar[2] = omit(arpeggio7('c`', 'f``', 'D Minor'), 3, 5)
         bar[3] = omit(dominant7('b', 'f``', 'G Major'), 3, 5)
-        bar[4] = bar[1]
+        bar[4] = deepcopy(bar[1])
 
         bar[5] = omit(arpeggio('c`', 'a``', 'A Minor'), 4)
         bar[6] = ['c`'] + arpeggio('d`', 'd``', 'D Major')
@@ -58,31 +46,11 @@ class PreludeInC(Piece):
         bar[28] = ['g,'] + omit(diminished7('ef', 'gf`', 'A Major'), 2, 5)
 
         bar[29] = omit(self.arpeggio('g,', 'g`'), 2, 6)
-        bar[30] = bar[26]
-        bar[31] = bar[27]
+        bar[30] = deepcopy(bar[26])
+        bar[31] = deepcopy(bar[27])
         bar[32] = omit(self.dominant7('c,', 'e`'), 2, 3, 4, 6, 9)
 
-        for c in bar:
-            if c:
-                motif(c)
-
-        def held_bass(tones):
-            tones = tonify(tones)
-            return voices(rest(16) + tied_note(tones[1], ['8.', 4, 2]), note(tones[0], 1))
-
-        def long_melody(tones):
-            tones = tonify(tones)
-            return rest(8) + notes(pattern(tones, 1, 2, 3, 4, 3, 2, 3, 2, 1, 2), 16)
-
-        self.score['treble'] += long_melody(arpeggio('f', 'f`', 'F Major')) + notes('f d', 16) * 2
-        self.score['treble'] += long_melody(dominant7('g`', 'f``', 'G Major')) + pattern(self.scale('d`', 'f`', 16), 1, 3, 2, 1)
-        self.score["bass"] += held_bass('c, c') + held_bass('c, b,')
-
-        self.score['treble'] += chord(self.arpeggio('e`', 'c``'), 1)
-        self.score['bass'] += chord('c, c', 1)
-
-    def end_score(self):
-        return ('>>\n  \\layout {\n \\context {\n \\Score\n \\override SpacingSpanner.common-shortest-duration =\n #(ly:make-moment 1/8)\n }\n }\n }')
+        return bar
 
 
 if __name__ == "__main__":
