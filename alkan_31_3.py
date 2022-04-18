eaea2b471a5e0c509022705b746bc51aa9ea930e
thomasmorgan <thomas.j.h.morgan@gmail.com>
2022-04-17 21:12:23 -0700

    alkan 31, 3, horrendous 3rd bar

--- alkan_31_3.py
+++ alkan_31_3.py
@@ -1,8 +1,8 @@
 from piece import Piece
-from points import notes, tied_note
+from points import notes, tied_note, chords, rests
 from staves import Treble, Super
 from markup import voices
-from util import select
+from util import select, omit
 
 
 class GenreAncien(Piece):
@@ -23,6 +23,11 @@ class GenreAncien(Piece):
         self.auto_add_bars = True
         self.key = 'bf minor'
 
+    def subtext(self):
+        return (
+          "\\layout { \\context { \\PianoStaff "
+          "\\consists #Span_stem_engraver } }\n")
+
     def write_score(self):
 
         # section 1
@@ -30,18 +35,33 @@ class GenreAncien(Piece):
         s1_treble1 = notes('af` df`` f``', [2, 4, 4])
         s1_treble2 = notes('f`` ef`` gf``', [2, 4, 4])
         s1_treble2b = self.scale('bf`', -4, 8) + self.scale('gf`', -4, 8)
+        s1_treble3 = notes('gf`` ef`` df`` c``', 4)
+        s1_treble3b = (
+            rests(8, prefix='\\omit ') + notes('ef`', 8) +
+            rests(2, 4, prefix='\\omit '))
 
         select(s1_treble1, 1).prefix += '\\tempo "Molto lento" '
         select(s1_treble1, 1).markdown = '\\italic{piacévole}'
+        select(s1_treble3b, 2).prefix = (
+            '\\voiceOne \\autoBeamOff \\stemDown \\crossStaff {')
+        select(s1_treble3b, 2).suffix = '} \\autoBeamOn \\stemNeutral '
 
         # bass
         s1_bass1 = self.scale('f`', -8, 8)
         s1_bass2 = tied_note('gf', [2, 8]) + self.scale('ef', 3, 8)
+        s1_bass3 = (
+            chords(['af c`', 'c`'], 8) +
+            self.harmonize(self.scale('df`', 4, 8) +
+                           self.scale('f`', -2, 8), 2))
+        select(s1_bass3, 3).prefix = '\\change Staff = "treble" \\voiceTwo '
+        s1_bass4 = rests(1)
 
         # put it all together
         self.score = {
-            'treble': s1_treble1 + voices(s1_treble2, s1_treble2b),
-            'bass': s1_bass1 + s1_bass2
+            'treble': (
+                s1_treble1 + voices(s1_treble2, s1_treble2b) +
+                voices(s1_treble3, s1_treble3b)),
+            'bass': s1_bass1 + s1_bass2 + s1_bass3 + s1_bass4
         }
 
 
